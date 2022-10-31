657ca4a9ce124ea85e1b01a1d1dff31e42fabaf1
Yusha47 <abdullahyusha47@gmail.com>
2022-10-31 22:04:17 +0600

    new changes

---
+++ Algorithms/Sieve of eratosThenes/sieve_variation_highest_lowest_prime.cpp
@@ -0,0 +1,59 @@
+//sieve lowest and highest prime of a number finder
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=1e7+10;
+    vector<bool> is_prime(n+1, true);
+    vector<int>lp(n,0),hp(n,0);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (is_prime[i])
+        {
+            lp[i]=hp[i]=i;
+            for (int j = i * i; j <= n; j += i)
+            {
+
+
+                is_prime[j] = false;
+                hp[j]=i;
+                if(lp[j]==0)
+                {
+                    lp[j]=i;
+                }
+            }
+        }
+    }
+
+    //prime factorization
+
+    int num;
+    cin>>num;
+    vector<int>prime_factors;
+    //map<int,int>prime_factors; for counting how many times are there 24 2(3times) 3 1 times;
+    while(num>1)
+    {
+        int prime_factor=hp[num];
+        while(num%prime_factor==0)
+        {
+            num/=prime_factor;
+            prime_factors.push_back(prime_factor);
+            //prime_factors[prime_factor]++;
+        }
+    }
+    for(int factor : prime_factors)
+    {
+         cout<<factor<<"\n";
+    }
+
+    // for counting how many times are there 24 2(3times) 3 1 times;
+    for(auto factor : prime_factors)
+    {
+        cout<<factor.first<< " "<<factor.second<<"\n";
+    }
+
+
+
+}
---
+++ Algorithms/Sieve of eratosThenes/sieve_variation_sieve.cpp
@@ -0,0 +1,33 @@
+//sieve lowest and highest prime of a number finder
+
+#include<bits/stdc++.h>
+using namespace std;
+const int n=1e5+10;
+vector<int> divisors[n];//.
+int main()
+{
+
+
+    for (int i = 2; i <n; i++)
+    {
+
+        for (int j = i ; j < n; j += i)
+        {
+            divisors[j].push_back(i);
+        }
+    }
+
+
+    //prime factorization
+    for(int i=1; i<10; i++)
+    {
+        for(int div : divisors[i])
+        {
+            cout<<div<< " ";
+        }
+        cout<<"\n";
+    }
+
+
+}
+
